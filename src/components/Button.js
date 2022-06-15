7f57e75cee7e43d897a1f847bc0539501eca181b
Fburbaud <fred.brechet@gmail.com>
2022-06-15 11:00:42 +0200

    Provider Consumer done

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,34 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
 
-    //hookup a context object to a class component
-    static contextType = LanguageContext;
+    //hookup a context object to a class component when using this.context
+    //static contextType = LanguageContext;
+
+    renderSubmit(value) {
+        return value === 'english' ? 'Submit' : 'Voorleggen';
+    }
+
+    renderButton(color) {
+        return(
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {(value) => this.renderSubmit(value)}
+                </LanguageContext.Consumer>
+            </button>
+        );
+    }
 
     render() {
 
-        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+        //const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
 
         return(
-            <button className="ui button primary">{text}</button>
+            <ColorContext.Consumer>
+                {(color) => this.renderButton(color)}
+            </ColorContext.Consumer>
         )
     }
 }
